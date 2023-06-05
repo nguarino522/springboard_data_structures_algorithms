598c4d9502992d859fd56f890c6de45104beeabd
nguarino522 <sub4miler5@gmail.com>
2023-06-05 19:58:58 -0400

    complete divide and conquer exercise

--- prob-divide-conquer/find-floor.js
+++ prob-divide-conquer/find-floor.js
@@ -1,5 +1,21 @@
-function findFloor() {
-  
+function findFloor(arr, num, firstIdx = 0, lastIdx = arr.length - 1) {
+    if (firstIdx > lastIdx) return -1;
+    if (num >= arr[lastIdx]) return arr[lastIdx];
+
+    let midIdx = Math.floor((firstIdx + lastIdx) / 2);
+
+    if (arr[midIdx] === num) return arr[midIdx];
+    
+    if (midIdx > 0 && arr[midIdx - 1] <= num && num < arr [midIdx]) {
+        return arr[midIdx -1];
+    }
+
+    if (num < arr[midIdx]) {
+        return findFloor(arr, num, firstIdx, midIdx -1);
+    }
+
+    return findFloor(arr, num, midIdx + 1, lastIdx);
+
 }
 
 module.exports = findFloor
--- prob-divide-conquer/find-rotation-count.js
+++ prob-divide-conquer/find-rotation-count.js
@@ -1,5 +1,56 @@
-function findRotationCount() {
-  
+function findRotationCount(arr, firstIdx = 0, lastIdx = arr.length - 1) {
+    if (lastIdx < firstIdx) {
+        return 0;
+    }
+
+    if (lastIdx === firstIdx) {
+        return firstIdx;
+    }
+
+    let midIdx = Math.floor((firstIdx + lastIdx) / 2);
+
+    // Check if element (mid+1) is minimum element.
+    // Consider the cases like [3, 4, 5, 1, 2]
+    if (midIdx < lastIdx && arr[midIdx + 1] < arr[midIdx]) {
+        return midIdx + 1;
+    }
+
+    // Check if mid itself is minimum element
+    if (midIdx > firstIdx && arr[midIdx] < arr[midIdx - 1]) {
+        return midIdx;
+    }
+
+    // Decide whether we need to go to left half or right half
+    if (arr[lastIdx] > arr[midIdx]) {
+        return findRotationCount(arr, firstIdx, midIdx - 1);
+    }
+
+    return findRotationCount(arr, midIdx + 1, lastIdx);
 }
+
+
+function findRotationCount2(array) {
+    // Check if the array is not rotated at all.
+    if (array[0] < array[array.length - 1]) {
+      return 0;
+    }
+  
+    // Find the index of the smallest element in the array using binary search.
+    let low = 0;
+    let high = array.length - 1;
+    let mid;
+    while (low <= high) {
+      mid = Math.floor((low + high) / 2);
+      if (array[mid] < array[0]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+  
+    // Return the number of rotations.
+    return low;
+  }
+
 
 module.exports = findRotationCount
